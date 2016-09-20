81faed0650160149b0c5feab484d3529189b49dc
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro.local>
2016-09-19 17:38:54 -0700

    Added new photos for some of the projects...
    * Added new photos for some of the projects to make them more uniform
    * Updated some styling for font-size and font-color
    * Added some additional social media links
    * Changed the copy on the About Me Page
    * Fixed the Menu button so that it closes when a new section is clicked

--- assets/main.js
+++ assets/main.js
@@ -9,9 +9,9 @@ var section_show_time = 1300;
 jQuery(document).ready(function($) {
 
   // Switch section
-  $("a", '.mainmenu').click(function() 
-  {
-    if( ! $(this).hasClass('active') ) { 
+  $("a", '.mainmenu').click(function(){
+    $(".dropdown").removeClass('open');
+    if(!$(this).hasClass('active')){ 
       current_item = this;
       // close all visible divs with the class of .section
       $('.section:visible').fadeOut( section_hide_time, function() { 
@@ -19,7 +19,7 @@ jQuery(document).ready(function($) {
         $(current_item).addClass( 'active' );
         var new_section = $( $(current_item).attr('href') );
         new_section.fadeIn( section_show_time );
-      } );
+      });
     }
     return false;
   });   
